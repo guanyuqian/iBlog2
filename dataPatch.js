c47b174b47b9799fd32eaa07bfc5d20a98d21181
guanyuqian <the_sam@foxmail.com>
2017-09-17 17:18:14 +0800

    fix:data patch for travels

--- dataPatch.js
+++ dataPatch.js
@@ -1,19 +1,40 @@
-
 /**
  * Created by the_s on 2017/9/11.
  */
 var category = require('./proxy/category');
-var travels={
+var travels = {
     "uniqueid": "Travels",
     "alias": "Travels",
     "catename": "Travels",
     "img": "/images/travel planning.png",
-    "link":''
+    "link": ''
 };
-category.save([travels], function (err) {
+
+/**
+ * 初始插入travels分類
+ */
+category.getAll(false, false, function (err, data) {
     if (err) {
-        console.log('err')
+        console.log('getAll err');
     } else {
-        console.log('travels patched')
+        var haveTravels = false;
+        data.forEach(function (item) {
+            if (item.Alias == 'Travels') {
+                haveTravels = true;
+                console.log('have travels category');
+                return;
+            }
+        });
+        if (haveTravels == false) {
+            console.log('insert travels category');
+            data.push(travels);
+            category.save(data, function (err) {
+                if (err) {
+                    console.log('err')
+                } else {
+                    console.log('travels patched')
+                }
+            });
+        }
     }
 });
