05658ee7764693cdba216175d30a1d404513b529
guanyuqian <the_sam@foxmail.com>
2017-09-17 22:37:49 +0800

    fix:travels mark offset

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -58,11 +58,11 @@ var myMap = {
  */
 
 const BAIDU_ICON_DEFAULT = new BMap.Icon("http://api.map.baidu.com/img/markers.png", new BMap.Size(23, 25), {
-    offset: new BMap.Size(10, 25), // 指定定位位置
+    anchor: new BMap.Size(10, 25), // 指定定位位置
     imageOffset: new BMap.Size(0, 0 - 11 * 25) // 设置图片偏移
 });
 const BAIDU_ICON_FOCUS = new BMap.Icon("http://api.map.baidu.com/img/markers.png", new BMap.Size(23, 25), {
-    offset: new BMap.Size(10, 25), // 指定定位位置
+    anchor: new BMap.Size(10, 25), // 指定定位位置
     imageOffset: new BMap.Size(0, 0 - 10 * 25) // 设置图片偏移
 });
 var DEFAULT_FOOT_SVG = new BMap.Symbol('M563.4 623.8c0.8-13.8 16.6-9 16.6-9l124 25.2 256 97.4c0-48-7.6-53-18.8-61.4L576 414c0 0-9.8-120-9.8-225.8 0-49-23.6-156.2-54.2-156.2s-54.2 108.8-54.2 156.2c0 100.4-9.8 225.8-9.8 225.8L82.8 676c-14.2 10-18.8 15.4-18.8 61.4L320 640l123.8-25.2c0 0 15.8-4.8 16.6 9 0.8 13.8-2.4 138.2 11.8 204.2 1.8 8.8-5 9.4-9.6 14.8l-103.8 65.6c-3.4 3.8-5 14.6-5 14.6l-2 37 136-32 24 64 24-64 136 32-2-37c0.2 0-1.4-10.8-4.8-14.6l-103.8-65.6c-4.6-5.4-11.4-6-9.6-14.8C565.4 762 562.6 637.6 563.4 623.8z', {
